5ada5d30eb06e46a276005f29a6a334390e59b35
099brock <bingham572@student.pcsstn.net>
2020-01-09 15:28:09 +0000

    finished code

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+/*
+  if the number ends in one then use the ordinal st
+*/
 
 int main(void) {
-  printf("Ordinal Numbers\n");
+ 
+ unsigned int max;
+ printf("max: ");
+ scanf("%u", &max);
+
+for (int i = 1; i <= max; i++)
+{
+  switch(i % 100) {
+    case 11:
+    case 12:
+    case 13:
+     printf("%ith\n", i);
+     continue;
+  }
+  switch(i % 10)
+  {
+    case 1:
+    printf("%ist\n", i);
+    continue;
+    case 2:
+    printf("%ind\n", i);
+    continue;
+    case 3:
+    printf("%ird\n", i);
+    continue;
+    default:
+    printf("%ith\n", i);
+    continue;
+  }
+}
   return 0;
 }
